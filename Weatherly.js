c6d717052046a5ba13803f6c7f9eb28c555a6c6d
SheaMaynard <weirdalgurl@yahoo.com>
2016-09-22 20:57:15 -0400

    zip code and dark sky working

--- Weatherly.js
+++ Weatherly.js
@@ -1,8 +1,34 @@
+
+
 function lookupLatLong_Complete(result) {
             var latitude = result.results[0].geometry.location.lat;
             var longitude = result.results[0].geometry.location.lng;
             console.log("The lat and long is " + latitude + ", " + longitude);
+            getWeather(latitude, longitude);
         }
+
+function getWeather_Complete(result) {
+    var temperature = result.currently.temperature;
+    var summary = result.currently.summary;
+    var rainChance = result.currently.precipProbability;
+    var icon = result.currently.icon;
+    console.log("This is the weather info " + temperature+ "," + summary+ "," +rainChance+ "," +icon)
+
+}        
+
+function getWeather(latitude, longitude){
+            var DarkskyUrl = "https://api.darksky.net/forecast/cbd31f8cd1b7e93ea299715eddb44f5e/" +latitude+ "," +longitude+"";
+            
+            var weather = {
+                url: DarkskyUrl,
+                dataType: "jsonp",
+                success: getWeather_Complete
+            };
+
+            $.ajax(weather);
+
+}
+
 
         function lookupLatLong(city, state, inputTextZip) {
             // Create the address.
@@ -25,6 +51,7 @@ function lookupLatLong_Complete(result) {
             };
 
             $.ajax(request);
+
         }
 
         function lookupWeatherForPostalCode_Click() {
